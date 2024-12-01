71d520af607aeb3e13f5c4f0c26f90f9e152ed15
Alex Pison <pisonalex@gmail.com>
2024-12-01 08:31:31 +0100

    Comment for LabSheet3.hs

--- LabSheet3.hs
+++ LabSheet3.hs
@@ -12,12 +12,15 @@ evenList :: [Int] -> Bool
 evenList = trueList . map even
 
 maxList :: (Ord a) => [a] -> a
--- maxList xs = foldr max (head xs) xs
-maxList = foldr1 max
+maxList xs = foldr max (head xs) xs
+
+-- Can be also done using foldr1 - then the first element of folding is the head
+-- maxList = foldr1 max
 
 inRange :: Ord a => a -> a -> [a] -> [a]
 inRange up low = filter (>=low) . filter (<=up)
 
+-- Maks' version of inRange using a lambda function
 inRange2 :: Ord a => a -> a -> [a] -> [a]
 inRange2 up low = foldr (\x ans -> if x>=low && x<=up then x:ans else ans) [] 
 
