e9384796bc40e754b9082192492f9fd35fc8967a
ALEXLAPTOP\pison <pisonalex@gmail.com>
2025-01-02 15:07:32 +0100

    NimGame added

--- LabSheet5.hs
+++ LabSheet5.hs
@@ -2,7 +2,7 @@
 -- post fac n = n!
 fac :: Int -> Int
 fac 0 = 1
-fac n = n * fac (n-1)
+fac n = n * fac (n - 1)
 
 -- base: n = 0 -> fac 0 = 0! -> fac 0 = 1
 -- Assume fac k = k! we need to show that fac (k+1) == (k+1!)
@@ -16,7 +16,7 @@ fac n = n * fac (n-1)
 fib :: Int -> Int
 fib 0 = 0
 fib 1 = 1
-fib n = fib (n-1) + fib (n-2)
+fib n = fib (n - 1) + fib (n - 2)
 
 -- base: n = 0 => fib 0 = 0 and fib 1 = 1
 -- fib 2 = fib 0 + fib 1 = 2
@@ -24,12 +24,11 @@ fib n = fib (n-1) + fib (n-2)
 -- We will try to show that fib (k+1) is true as well
 -- fib (k+1) = fib k + fib (k-1)
 
-
 -- pre: True
 -- post: reverse xs = reversed xs,
 reverse'' :: [a] -> [a] -> [a]
 reverse'' [] sx = sx
-reverse'' (x:xs) sx = reverse'' xs (x:sx)
+reverse'' (x : xs) sx = reverse'' xs (x : sx)
 
 -- Base: reverse [] = []
 -- Assume reverse [k] = reversed [k]
@@ -39,5 +38,38 @@ reverse'' (x:xs) sx = reverse'' xs (x:sx)
 reverse' :: [a] -> [a]
 reverse' xs = reverse'' xs []
 
+displayBoard :: [Int] -> IO ()
+displayBoard = print
+
+emptyBoard :: [Int] -> Bool
+emptyBoard = foldr ((&&) . (== 0)) True
+
+makeMove :: [Int] -> Int -> Int -> [Int]
+makeMove board row num = take (row - 1) board ++ [board !! (row - 1) - num] ++ drop row board
+
+play :: [Int] -> Bool -> IO ()
+play board isP1 = do
+  displayBoard board
+  if isP1
+    then putStrLn "player 1's turn"
+    else putStrLn "player 2's turn"
+  putStr "enter heap: "
+  input1 <- getLine
+  let row = (read input1 :: Int)
+  putStr "enter number to take: "
+  input2 <- getLine
+  let num = (read input2 :: Int)
+
+  let newBoard = makeMove board row num
+
+  if emptyBoard newBoard
+    then
+      if isP1
+        then putStrLn "player 2 wins"
+        else putStrLn "player 1 wins"
+    else play newBoard (not isP1)
+
 nimGame :: IO ()
-nimGame = do putStrLn "Hello"
+nimGame = do
+  putStrLn "welcome to nim"
+  play [1, 3, 5, 7] True
