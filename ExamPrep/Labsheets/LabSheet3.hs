745a708533a8c0e872e99261dd6048d010dba1e2
alex <pisonalex@gmail.com>
2025-04-27 22:24:32 +0100

    Implemented a lot of functionality using high order functions map, filter, and foldr

--- ExamPrep/Labsheets/LabSheet3.hs
+++ ExamPrep/Labsheets/LabSheet3.hs
@@ -1,3 +1,30 @@
 mult :: Num a => [a] -> a
 mult [] = 0
 mult xs = foldr (*) 1 xs
+
+posList :: [Int] -> [Int]
+posList = filter (>0)
+
+trueList :: [Bool] -> Bool
+trueList = foldr (&&) True
+
+evenList :: [Int] -> Bool
+evenList xs = length (filter even xs) == length xs
+
+evenList' :: [Int] -> Bool
+evenList' = foldr ((&&) . even) True
+
+maxList :: Ord a => [a] -> a
+maxList (x:xs) = foldr max x xs
+
+inRange :: Int -> Int -> [Int] -> [Int]
+inRange lo hi = filter (\x -> x >= lo && x <= hi)
+
+countPositives :: [Int] -> Int
+countPositives xs = length (filter (>0) xs)
+
+myLength :: [a] -> Int
+myLength = foldr ((+) . (\x -> 1)) 0
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap f = foldr ((:).f) []
